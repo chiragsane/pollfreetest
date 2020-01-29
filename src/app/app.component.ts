63fdf57f1a799e495ab238ccefeb740b1b7f47e0
Chirag Sane <chiragsane333@gmail.com>
2020-01-30 01:30:02 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pft-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pollfreetest';
+}
